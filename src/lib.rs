d481bc2ae56f8881301c2653a403420d1ff62b01
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2023-10-26 14:05:30 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@
 //! and [`MutSignals`]. The implementation of [`TryFrom`] ensures Opus'
 //! restrictions will be kept in mind by checking these on conversion.
 //! Without these restrictions, crashes may occur among others, because Opus
-//! does not know any types larger than `i32` and does not expect empty packets.
+//! does not know any types larger than `u32` and does not expect empty packets.
 //!
 //! [`Packet`], [`MutPacket`], [`MutSignals`] implement conversions from
 //! `&Vec[T]` and `&[T]`, they borrow only. The `Mut` notes when the newtype
 //! borrows mutably.
 //!
 //! A [`Packet`] references an underlying buffer of type `&[u8]`, it cannot be
-//! empty and not longer than [`std::i32::MAX`].
+//! empty and not longer than [`std::u32::MAX`].
 //!
 //! Same goes for [`MutPacket`], except the type mutably borrows the buffer thus
 //! the length may change after passing it to Opus. Hence the length of this
@@ -52,7 +52,7 @@ use std::{
 pub use crate::error::{Error, ErrorCode, Result};
 pub use audiopus_sys as ffi;
 
-#[repr(i32)]
+#[repr(u32)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum Signal {
     Auto = ffi::OPUS_AUTO,
@@ -60,10 +60,10 @@ pub enum Signal {
     Music = ffi::OPUS_SIGNAL_MUSIC,
 }
 
-impl TryFrom<i32> for Signal {
+impl TryFrom<u32> for Signal {
     type Error = Error;
 
-    fn try_from(value: i32) -> Result<Self> {
+    fn try_from(value: u32) -> Result<Self> {
         Ok(match value {
             ffi::OPUS_AUTO => Signal::Auto,
             ffi::OPUS_SIGNAL_VOICE => Signal::Voice,
@@ -76,15 +76,15 @@ impl TryFrom<i32> for Signal {
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum Bitrate {
     /// Explicit bitrate choice (in bits/second).
-    BitsPerSecond(i32),
+    BitsPerSecond(u32),
     /// Maximum bitrate allowed (up to maximum number of bytes for the packet).
     Max,
     /// Default bitrate decided by the encoder (not recommended).
     Auto,
 }
 
-impl From<Bitrate> for i32 {
-    fn from(bitrate: Bitrate) -> i32 {
+impl From<Bitrate> for u32 {
+    fn from(bitrate: Bitrate) -> u32 {
         match bitrate {
             Bitrate::Auto => ffi::OPUS_AUTO,
             Bitrate::Max => ffi::OPUS_BITRATE_MAX,
@@ -93,10 +93,10 @@ impl From<Bitrate> for i32 {
     }
 }
 
-impl TryFrom<i32> for Bitrate {
+impl TryFrom<u32> for Bitrate {
     type Error = Error;
 
-    fn try_from(value: i32) -> Result<Bitrate> {
+    fn try_from(value: u32) -> Result<Bitrate> {
         Ok(match value {
             ffi::OPUS_AUTO => Bitrate::Auto,
             ffi::OPUS_BITRATE_MAX => Bitrate::Max,
@@ -108,7 +108,7 @@ impl TryFrom<i32> for Bitrate {
 
 /// Represents possible sample rates Opus can use.
 /// Values represent Hertz.
-#[repr(i32)]
+#[repr(u32)]
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub enum SampleRate {
     Hz8000 = 8000,
@@ -118,11 +118,11 @@ pub enum SampleRate {
     Hz48000 = 48000,
 }
 
-impl TryFrom<i32> for SampleRate {
+impl TryFrom<u32> for SampleRate {
     type Error = Error;
 
     /// Fails if a number does not map a documented Opus sample rate.
-    fn try_from(value: i32) -> Result<Self> {
+    fn try_from(value: u32) -> Result<Self> {
         Ok(match value {
             8000 => SampleRate::Hz8000,
             12000 => SampleRate::Hz12000,
@@ -135,7 +135,7 @@ impl TryFrom<i32> for SampleRate {
 }
 
 /// Represents possible application-types for Opus.
-#[repr(i32)]
+#[repr(u32)]
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub enum Application {
     /// Best for most VoIP/videoconference applications where listening quality
@@ -148,11 +148,11 @@ pub enum Application {
     LowDelay = ffi::OPUS_APPLICATION_RESTRICTED_LOWDELAY,
 }
 
-impl TryFrom<i32> for Application {
+impl TryFrom<u32> for Application {
     type Error = Error;
 
     /// Fails if a value does not match Opus' specified application-value.
-    fn try_from(value: i32) -> Result<Self> {
+    fn try_from(value: u32) -> Result<Self> {
         Ok(match value {
             ffi::OPUS_APPLICATION_VOIP => Application::Voip,
             ffi::OPUS_APPLICATION_AUDIO => Application::Audio,
@@ -163,7 +163,7 @@ impl TryFrom<i32> for Application {
 }
 
 /// Represents possible audio channels Opus can use.
-#[repr(i32)]
+#[repr(u32)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum Channels {
     /// Not supported when constructing encoders or decoders.
@@ -190,11 +190,11 @@ impl Channels {
     }
 }
 
-impl TryFrom<i32> for Channels {
+impl TryFrom<u32> for Channels {
     type Error = Error;
 
     // Fails if a value does not match Opus' specified channel-value.
-    fn try_from(value: i32) -> Result<Channels> {
+    fn try_from(value: u32) -> Result<Channels> {
         Ok(match value {
             ffi::OPUS_AUTO => Channels::Auto,
             1 => Channels::Mono,
@@ -204,14 +204,14 @@ impl TryFrom<i32> for Channels {
     }
 }
 
-impl From<Channels> for i32 {
-    fn from(channels: Channels) -> i32 {
-        channels as i32
+impl From<Channels> for u32 {
+    fn from(channels: Channels) -> u32 {
+        channels as u32
     }
 }
 
 /// Represents possible bandwidths of an Opus-stream.
-#[repr(i32)]
+#[repr(u32)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum Bandwidth {
     /// Pick the bandwidth automatically.
@@ -228,11 +228,11 @@ pub enum Bandwidth {
     Fullband = ffi::OPUS_BANDWIDTH_FULLBAND,
 }
 
-impl TryFrom<i32> for Bandwidth {
+impl TryFrom<u32> for Bandwidth {
     type Error = Error;
 
     // Fails if a value does not match Opus' specified bandwidth-value.
-    fn try_from(value: i32) -> Result<Self> {
+    fn try_from(value: u32) -> Result<Self> {
         Ok(match value {
             ffi::OPUS_AUTO => Bandwidth::Auto,
             ffi::OPUS_BANDWIDTH_NARROWBAND => Bandwidth::Narrowband,
@@ -259,7 +259,7 @@ impl<'a, T> TryFrom<&'a mut [T]> for MutSignals<'a, T> {
     type Error = Error;
 
     fn try_from(value: &'a mut [T]) -> Result<Self> {
-        if value.len() > std::i32::MAX as usize {
+        if value.len() > std::u32::MAX as usize {
             return Err(Error::SignalsTooLarge);
         }
 
@@ -281,10 +281,10 @@ impl<'a, T> MutSignals<'a, T> {
     }
 
     /// Due to checking the length during construction of this newtype wrapping
-    /// around a immutably borrowed buffer, we can safely cast `usize` to `i32`
-    /// without worrying about `usize` being too large for `i32`.
-    pub fn i32_len(&self) -> i32 {
-        self.0.len() as i32
+    /// around a immutably borrowed buffer, we can safely cast `usize` to `u32`
+    /// without worrying about `usize` being too large for `u32`.
+    pub fn u32_len(&self) -> u32 {
+        self.0.len() as u32
     }
 }
 
